9e1651bb01769b531042d37bb38a55d519e398f7
Colin McGregor <100391145+colinwmc@users.noreply.github.com>
2024-12-07 12:23:02 -0600

    riddle system up and working

--- Text_Game_Frontend/src/app/riddle-teller/riddle-teller.component.ts
+++ Text_Game_Frontend/src/app/riddle-teller/riddle-teller.component.ts
@@ -19,6 +19,14 @@ export class RiddleTellerComponent implements OnInit {
   public backpackOpen = false;
   public npcTag = 'Temple Guard: ';
   public hasAcknowledgedPoop = false;
+  public riddleArray = [
+    { question: '"What gets wetter the more it dries?"', options: [{ id: 20, text: 'b---d' }, { id: 22, text: 't---l' }, { id: 20, text: 'c----t' }, { id: 20, text: 'h--r' }] },
+    { question: '"What tree do we all carry in our hands?"', options: [{ id: 20, text: 'c---r' }, { id: 20, text: 'o-k' }, { id: 20, text: 'e--------s' }, { id: 23, text: 'p--m' }] },
+    { question: '"I can\'t be seen, but I can be heard. I won\'t answer until spoken to. What am I?"', options: [{ id: 20, text: 'd---m' }, { id: 20, text: 'c------n' }, { id: 24, text: 'e--o' }, { id: 20, text: 'v---e' }] },
+  ];
+  public riddleIndex = 0;
+  public rightAnswers = 0;
+  public wrongAnswers = 0;
 
   constructor(private sharedService: SharedService, private router: Router) { }
 
@@ -30,9 +38,9 @@ export class RiddleTellerComponent implements OnInit {
 
     } else {
       this.sharedService.getPCList().subscribe(data => {
-        this.PC = data[0];
+        this.PC = data[1];
         this.PC.currentHealth = this.PC.hp;
-        this.PC.hasShitPants = false;
+        this.PC.hasShitPants = true;
         this.sharedService.PC = this.PC;
         this.resetPC = JSON.parse(JSON.stringify(this.PC));
       })
@@ -50,6 +58,9 @@ export class RiddleTellerComponent implements OnInit {
     this.backpackOpen = false;
     this.npcTag = 'Temple Guard: ';
     this.hasAcknowledgedPoop = false;
+    this.riddleIndex = 0;
+    this.rightAnswers = 0;
+    this.wrongAnswers = 0;
   }
 
   addPCDialogue(text: string) {
@@ -142,6 +153,25 @@ export class RiddleTellerComponent implements OnInit {
           { id: 11, text: '"My business is my own, wretched thing."' }
         ];
         break;
+      case 10:
+        this.addPCDialogue(event.text);
+        this.npcDialogue('"And I shall grant entrance to thee if you first answer my riddles three."');
+        this.options = [
+          { id: 13, text: '"Oh gods . . . you can\'t be serious."' },
+          { id: 14, text: '"Ok . . ."' },
+          { id: 15, text: '"Sounds like fun."' }
+        ]
+        break;
+      case 11:
+        this.addPCDialogue(event.text);
+        let dialogue = '"I assure you I don\'t give a jackalope\'s fart about your *business';
+        this.PC.hasShitPants ? dialogue += ',* though I am aggrieved that you\'ve made me smell it.' : dialogue += '.*';
+        dialogue += ' All I want to know is whether you intend to go through this door."'
+        this.npcDialogue(dialogue);
+        this.options = [
+          { id: 10, text: '"Yes. I need to enter the temple."' }
+        ]
+        break;
       case 12:
         this.addPCDialogue(event.text);
         this.npcDialogue('"No, no, not me. But then again perhaps yes. Tell me first, for what reason you stand before me."');
@@ -150,7 +180,59 @@ export class RiddleTellerComponent implements OnInit {
           { id: 11, text: '"My business is my own, wretched thing."' }
         ];
         break;
+      case 13:
+      case 14:
+      case 15:
+        this.addPCDialogue(event.text);
+        let intro = event.id === 13 ? '"Oh, I most certainly am. For this is my solemn oath. ' : event.id === 14 ? '"' : '"Yes, indeed, but fun for whom? That\'s the question. ';
+        this.npcDialogue(intro + 'Now, the rules are very simple. I will ask you a series of questions. If you can answer three corectly, I will allow you safe passage into this temple. However, if you answer three incorrectly, I will eat you."');
+        this.options = [
+          { id: 16, text: '"You\'re going to *eat* me? Why would you eat me?"' },
+          { id: 17, text: '"Alright. Let\'s get started I suppose."' },
+          { id: 18, text: '"Sigh . . . can\'t we just fight or something?"' }
+        ]
+        break;
+      case 16:
+      case 17:
+      case 18:
+        this.addPCDialogue(event.text);
+        let start = event.id === 16 ? '"Because these are the rules, and I also I didn\'t eat dinner. ' : event.id === 17 ? '"Stupendous. ' : '"Absolutely not. How barbaric. ';
+        this.npcDialogue(start + 'Now, for the first question:');
+        this.prepareRiddles(this.riddleArray);
+        this.options = [{ id: 19, text: 'Continue >' }];
+        break;
+      case 19:
+        this.npcDialogue(this.riddleArray[this.riddleIndex].question);
+        this.options = this.riddleArray[this.riddleIndex].options;
+        break;
+      case 20:
+        this.riddleIndex++;
+        this.wrongAnswers++;
+        if (this.wrongAnswers === 1) {
+          this.npcDialogue('"Oh dear, that\'s incorrect. One strike, but not to worry. On to the next."');
+          this.options = [{ id: 19, text: 'Continue >' }];
+        } else if (this.wrongAnswers === 2) {
+          this.npcDialogue('"Incorrect again, I\'m afraid. That\'s two out of three. Starting to sweat a bit, I wonder? Here\'s your next question, best think carefully."');
+          this.options = [{ id: 19, text: 'Continue >' }];
+        } else {
+          this.npcDialogue('"I\'m sorry to say that\'s again incorrect old chap. Well, we both know what happens now." The creature swoops down from their perch at lightning speed. It\'s jaw unhinges as you stare into the dark abyss of its mouth.');
+          this.options = [{ id: 21, text: '"AAAAHAHHHH!' }];
+        }
+        break;
+      case 21:
+        this.sharedService.takeDamage(this.PC.hp);
+        break;
     }
+  }
+
+  prepareRiddles(array: any) {
+
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+
   }
 
 }
